4af89a61b37ca2368701eea3402ad726b7b462d4
MarcosDMGConsultoria <marcos.fernandes@aluno.faculdadeimpacta.com.br>
2021-03-21 01:10:46 -0300

    Adcionando componentes para pagina home

--- Front-End/IJeans/src/app/home/home.module.ts
+++ Front-End/IJeans/src/app/home/home.module.ts
@@ -2,10 +2,16 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
+import { ProdutoComponent } from './produto/produto.component';
+import { FuncionarioComponent } from './funcionario/funcionario.component';
+import { FornecedorComponent } from './fornecedor/fornecedor.component';
 
 @NgModule({
     declarations: [
-        HomeComponent
+        HomeComponent,
+        ProdutoComponent,
+        FuncionarioComponent,
+        FornecedorComponent
     ],
     imports: [
         CommonModule,
