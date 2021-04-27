c53cb8ba4701ba7a15791285d35b2aa63d46248d
MarcosDMGConsultoria <marcos.fernandes@aluno.faculdadeimpacta.com.br>
2021-04-26 21:02:35 -0300

    tipoProduto changes size

--- Front-End/IJeans/src/app/cadastro/produto/cadastro-produto.component.ts
+++ Front-End/IJeans/src/app/cadastro/produto/cadastro-produto.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { MarcaModel } from "src/app/shared/model/marca.model";
 import { ProdutoModel } from "src/app/shared/model/produto.model";
@@ -9,99 +9,125 @@ import { ProdutoService } from "../../home/produto/service/service-produto.servi
 
 
 @Component({
-    selector: 'app-cadastraproduto',
-    templateUrl: './cadastro-produto.component.html',
-    styleUrls: ['./cadastro-produto.component.css']
-  })
+  selector: 'app-cadastraproduto',
+  templateUrl: './cadastro-produto.component.html',
+  styleUrls: ['./cadastro-produto.component.css']
+})
 
 
-export class CadastrarProdutosComponent{
+export class CadastrarProdutosComponent {
 
-  cadastraProdutoForm : FormGroup;
+  cadastraProdutoForm: FormGroup;
+  
 
-  constructor(private formBuilder :  FormBuilder, private service : ProdutoService,private router: Router) {
+  constructor(private formBuilder: FormBuilder, private service: ProdutoService, private router: Router) {
   }
 
   marcas: Array<MarcaModel>
-  marca:MarcaModel
-  tamanhos:Array<TamanhoModel>
-  tamanho:TamanhoModel
-  tipoProdutos:Array<TipoProduto>
-  tipoProduto:TipoProduto
+  marca: MarcaModel
+  tamanhos: Array<TamanhoModel>
+  tamanho: TamanhoModel
+  tipoProdutos: Array<TipoProduto>
+  tipoProduto: TipoProduto
+  tipoProdutoControl:FormControl
+  
+  idTipoProduto="1";
 
   ngOnInit() {
-    
-    this.cadastraProdutoForm = this.formBuilder.group( { 
-      nome : [ '',[Validators.required]  ], 
-      valor : [ '' , [Validators.required,Validators.pattern('^[0-9]')] ],
-      quantidade : [ '' , [Validators.required,Validators.pattern('^[0-9]')], ]
+
+    this.tipoProdutoControl = new FormControl('valid', [
+      Validators.required,
+      Validators.pattern('valid'),
+    ]);
+
+    this.cadastraProdutoForm = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      valor: ['', [Validators.required, Validators.pattern('^[0-9]')]],
+      quantidade: ['', [Validators.required, Validators.pattern('^[0-9]')],],
+      tipoProduto:['', [Validators.required]]
 
     });
-    this.service.findAllMarcas().subscribe(data =>{
-      this.marcas =  data
+    this.service.findAllMarcas().subscribe(data => {
+      this.marcas = data
       this.marca = data[0]
-    })
-    this.service.findAllTamanhos().subscribe(data => {
-      this.tamanhos = data
-      this.tamanho= data[0]
     })
     this.service.findAllTipoProduto().subscribe(data => {
       this.tipoProdutos = data
       this.tipoProduto = data[0]
+      this.service.findByTipoProduto(this.tipoProduto.id).subscribe(data => {
+        this.tamanhos = data
+        this.tamanho = data[0]
+      })
     })
+    // this.service.findAllTamanhos().subscribe(data => {
+    //   this.tamanhos = data
+    //   this.tamanho= data[0]
+    // })
+
 
   }
-  
-  selectMarca(marca){
+
+  selectMarca(marca) {
     console.log(marca)
-    this.marca=marca
+    this.marca = marca
   }
-  selectTamanho(tamanho){
-    this.tamanho=tamanho
+  selectTamanho(tamanho) {
+    console.log(tamanho)
+    this.tamanho = tamanho
   }
-  selectTipoProduto(tamanho){
-    this.tamanho=tamanho
+  selectTipoProduto(tipo:TipoProduto) {
+    this.tipoProduto = tipo
+
+    if(this.idTipoProduto === "1"){
+      this.idTipoProduto = "2"
+    }else{
+      this.idTipoProduto = "1"
+    }
+    this.service.findByTipoProduto(this.idTipoProduto).subscribe(data => {
+      this.tamanhos = data
+      this.tamanho = data[0]
+    })
   }
 
-  onSubmit(){
+  onSubmit() {
     // Verifica ao enviar se os dados informados são validos
-   if(
-     this.cadastraProdutoForm.get('nome').valid &&
-     this.cadastraProdutoForm.get('valor').valid &&
-     this.cadastraProdutoForm.get('quantidade').valid
-    ){
-      let produto =  {
-        nome:this.cadastraProdutoForm.value.nome,
-        preco_atual:this.cadastraProdutoForm.value.valor,
-        quantidade_estoque:this.cadastraProdutoForm.value.quantidade,
-        marca:this.marca,
-        tipo_produto:this.tipoProduto,
-        tamanho:this.tamanho.id
-        }
-  
-  
-        this.service.cadastrarProduto(produto).subscribe(data => {
-          if(data.id != null){
-            alert("produto cadastrado com sucesso")
-            this.router.navigateByUrl('home');
-          }else{
-            alert("produto ja cadastrado");
-          }
-        })
-      }else{
-        alert('Dados Incorretos')
+    if (
+      this.cadastraProdutoForm.get('nome').valid &&
+      this.cadastraProdutoForm.get('valor').valid &&
+      this.cadastraProdutoForm.get('quantidade').valid
+    ) {
+      let produto = {
+        nome: this.cadastraProdutoForm.value.nome,
+        preco_atual: this.cadastraProdutoForm.value.valor,
+        quantidade_estoque: this.cadastraProdutoForm.value.quantidade,
+        marca: this.marca,
+        tipo_produto: this.tipoProduto,
+        tamanho: this.tamanho.id
       }
-    }
-   
-    
-    
-    voltarHome() {
-      this.router.navigateByUrl("home")
-    }
-   
-    isErrorCampo(nomeCampo){
-      return (!this.cadastraProdutoForm.get(nomeCampo).valid && this.cadastraProdutoForm.get(nomeCampo).touched ); 
+
+
+      this.service.cadastrarProduto(produto).subscribe(data => {
+        if (data.id != null) {
+          alert("produto cadastrado com sucesso")
+          this.router.navigateByUrl('home');
+        } else {
+          alert("produto ja cadastrado");
+        }
+      })
+    } else {
+      alert('Dados Incorretos')
     }
   }
+
+
+
+  voltarHome() {
+    this.router.navigateByUrl("home")
+  }
+
+  isErrorCampo(nomeCampo) {
+    return (!this.cadastraProdutoForm.get(nomeCampo).valid && this.cadastraProdutoForm.get(nomeCampo).touched);
+  }
+}
 
 
