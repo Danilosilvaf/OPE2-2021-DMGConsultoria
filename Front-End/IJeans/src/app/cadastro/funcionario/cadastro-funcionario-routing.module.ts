78a8f21031bafd03b0380b71311c6bfffdc559ea
MarcosDMGConsultoria <marcos.fernandes@aluno.faculdadeimpacta.com.br>
2021-05-30 18:56:20 -0300

    add guards

--- Front-End/IJeans/src/app/cadastro/funcionario/cadastro-funcionario-routing.module.ts
+++ Front-End/IJeans/src/app/cadastro/funcionario/cadastro-funcionario-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { GuardsService } from "src/app/shared/guards/guards.service";
 import { CadastrarFuncionarioComponent } from "./cadastro-funcionario.component";
 
 const routes: Routes = [
-    {path:'cadastrofuncionario', component: CadastrarFuncionarioComponent}
+    {path:'cadastrofuncionario', component: CadastrarFuncionarioComponent,canActivate:[GuardsService]}
      ];
 
 @NgModule({
--- Front-End/IJeans/src/app/home/estoque/movimentacao_estoque/saida/saida-routing.module.ts
+++ Front-End/IJeans/src/app/home/estoque/movimentacao_estoque/saida/saida-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { GuardsService } from "src/app/shared/guards/guards.service";
 import { SaidaComponent } from "./saida.component";
 
 const routes: Routes = [
-    {path:'saida/:id', component:SaidaComponent }
+    {path:'saida/:id', component:SaidaComponent,canActivate:[GuardsService] }
      ];
 
 @NgModule({
