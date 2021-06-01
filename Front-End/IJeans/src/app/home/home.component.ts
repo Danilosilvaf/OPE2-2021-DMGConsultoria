f1b4b8b39d8b5947255f279ef0a8b386666d7e66
Guilherme <guilherme.elias@aluno.faculdadeimpacta.com.br>
2021-05-31 22:44:47 -0300

    Botão logout

--- Front-End/IJeans/src/app/home/home.component.ts
+++ Front-End/IJeans/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { ServiceLoginService } from "../login/service/service-login.service";
 
 @Component({
     selector: 'app-home',
@@ -8,7 +9,7 @@ import { Router } from "@angular/router";
   })
 
 export class HomeComponent {
-    constructor(private router: Router) {
+    constructor(private router: Router, private serviceLogin: ServiceLoginService) {
         
     }
     
@@ -53,5 +54,9 @@ export class HomeComponent {
       this.displayFuncionario='none'
       this.displayMarca = 'none'
       this. displayEstoque='block'
+    }
+    logout(){
+      this.serviceLogin.logout();
+      this.router.navigateByUrl('');
     }
 }
