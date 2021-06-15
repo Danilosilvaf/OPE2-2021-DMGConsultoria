ef99d7fe8e8aecafe3c0b8f8091eb20c76b6488a
MarcosDMGConsultoria <marcos.fernandes@aluno.faculdadeimpacta.com.br>
2021-06-14 21:43:51 -0300

    asdada

--- Front-End/IJeans/src/app/enviroment/environment.ts
+++ Front-End/IJeans/src/app/enviroment/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  urlAPI: "ec2-18-230-6-253.sa-east-1.compute.amazonaws.com:5000/:5000",
+  urlAPI: "ec2-18-230-6-253.sa-east-1.compute.amazonaws.com:5000",
   storageKeysConfig : {
     localUser: 'localUser'
   }
